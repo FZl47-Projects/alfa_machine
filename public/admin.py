a779adeebba3de4297806f5f8e9ad5375d6df5d9
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-11-14 22:52:25 +0330

    Add TaskMaster model to admin

--- public/admin.py
+++ public/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Department)
+admin.site.register(models.TaskMaster)
 admin.site.register(models.Project)
 admin.site.register(models.Task)
 admin.site.register(models.ProjectFile)
 admin.site.register(models.InquiryFile)
-
