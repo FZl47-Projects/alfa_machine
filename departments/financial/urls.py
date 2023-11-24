9ecc3e9dc66b5c3bf8104564c74dab09fcdec2e7
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-11-25 00:34:29 +0330

    Add all projects_list to financial

--- departments/financial/urls.py
+++ departments/financial/urls.py
@@ -6,8 +6,9 @@ app_name = 'departments.financial'
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('project/list/', views.ProjectListView.as_view(), name='projects_list'),
     path('project/<int:project_id>', views.PaymentProject.as_view(), name='payment_project'),
     path('payment', views.Payment.as_view(), name='payment'),
     path('prepayment', views.PrePayment.as_view(), name='prepayment'),
-    path('/surety-bond/save/', views.SaveSuretyBondView.as_view(), name='save_surety_bond')
+    path('surety-bond/save/', views.SaveSuretyBondView.as_view(), name='save_surety_bond')
 ]
